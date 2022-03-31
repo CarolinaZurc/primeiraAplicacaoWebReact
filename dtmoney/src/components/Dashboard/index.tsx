979c0ac935793affcf254244415037af0317b0db
Carolina F Zurc <carolinafzurc@gmail.com>
2022-03-31 16:20:18 -0300

    alteração dashboard

--- dtmoney/src/components/Dashboard/index.tsx
+++ dtmoney/src/components/Dashboard/index.tsx
@@ -5,6 +5,7 @@ import { Summary } from '../Summary/index';
 export function Dashboard() {
     return (
         <Container>
+            <h1> ola</h1>
             <Summary />
         </Container>
     );
